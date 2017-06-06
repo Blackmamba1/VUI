67779b1409444c62111ad69607487cfbb36a1594
张经国 <zhangjg@asiainfo.com>
2017-06-06 19:02:12 +0800

    update markdown

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![npm package](https://img.shields.io/npm/v/easy-vui.svg)](https://www.npmjs.com/package/easy-vui)
 [![NPM downloads](https://img.shields.io/npm/dm/easy-vui.svg)](https://www.npmjs.com/package/easy-vui)
 
-> A Simple UI Component
+> A Simple UI Lib For Vue2.0
 
 ## Install
 
--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 /**
  * Created by zhangjingguo on 2017/6/5.
  */
-import Vue from 'vue'
-import * as vui from './src/lib'
 
-console.log(vui)
-
-const components = {
-  ...vui
-}
-
-console.log(vui, components)
-
-const install = (Vue, options) => {
-  Object.keys(components).forEach((key) => {
-    Vue.component(components[key].name, components[key])
-  })
-}
-Vue.use(install)
+module.exports = require('./src/lib')
